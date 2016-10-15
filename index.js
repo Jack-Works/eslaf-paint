32cc62217762335eab224750aeead5afe2723f13
Jack Works <zjwpeter@gmail.com>
2016-10-15 20:56:33 +0800

    Fix path problem

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const fs = require('fs')
 const Canvas = require('./lib/canvas.js')
 
 const argv = require('minimist')(process.argv.slice(2))
-console.log(process.argv)
 const Styles = require('./lib/style.js')(((css, _) => {
 	if (!css) throw new Error('No CSS File provided')
 	if (_) console.warn(`Only ${css} will be used to draw the image`)
-	return fs.readFileSync(css, 'utf-8')
+	return fs.readFileSync(path.join(process.cwd(), css), 'utf-8')
 })(...argv._.filter(v => path.extname(v) == '.css')))
 
-const text = require('./' + argv.text)
+const text = require(path.join(process.cwd(), argv.text))
 
 // for each img
 argv._.filter(v => path.extname(v) != '.css').forEach(image => {
@@ -38,7 +37,7 @@ argv._.filter(v => path.extname(v) != '.css').forEach(image => {
 		const outputPattern = profile.replace(/\$/g, base)
 		const base_out = path.basename(outputPattern)
 		const outputDir = path.dirname(outputPattern)
-		const final = path.join(outputDir, base_out + ext)
+		const final = path.join(process.cwd(), outputDir, base_out + ext)
 		if (!fs.existsSync(outputDir)){
 			console.log('Creating "' + outputDir + '" ...')
 			fs.mkdirSync(outputDir)
