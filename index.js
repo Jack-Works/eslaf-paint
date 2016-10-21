69e1ceb462822b551b62e14143177fe362d0413c
Jack Works <zjwpeter@gmail.com>
2016-10-21 18:16:31 +0800

    Now can import this package as a module
    Signed-off-by: Jack Works <zjwpeter@gmail.com>

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
-#!/usr/bin/env node
-'use strict'
-const merge = require('lodash.defaultsdeep')
-const path = require('path')
-const fs = require('fs')
+module.exports = argv => {
+	'use strict'
+	const merge = require('lodash.defaultsdeep')
+	const path = require('path')
+	const fs = require('fs')
 
-const Canvas = require('./lib/canvas.js')
-const {checkFile, checkSrcType} = require('./lib/check.js')
+	const Canvas = require('./lib/canvas.js')
+	const {checkFile, checkSrcType} = require('./lib/check.js')
 
-const argv = require('minimist')(process.argv.slice(2))
-const Styles = require('./lib/style.js')((
-	(css, _) => 
-		checkFile('No CSS file found')(css, _) ||
-		fs.readFileSync(css, 'utf-8'))
-	(...argv._.filter(checkSrcType('css'))))
+	const Styles = require('./lib/style.js')((
+		(css, _) => 
+			checkFile('No CSS file found')(css, _) ||
+			fs.readFileSync(css, 'utf-8'))
+		(...argv._.filter(checkSrcType('css'))))
 
-const config =
-	((c, _) =>
-		checkFile('No config file found')(c, _) ||
-		require(path.resolve(process.cwd(), c))
-	)
-	(...argv._.filter(checkSrcType('requirable')))
+	const config =
+		((c, _) =>
+			checkFile('No config file found')(c, _) ||
+			require(path.resolve(process.cwd(), c))
+		)
+		(...argv._.filter(checkSrcType('requirable')))
 
-const drawType = {
-	text: (canvasContext, {text, styles}) =>
-		canvasContext.drawText(text, styles, {stroke: !!styles.strokeWeight}),
+	const drawType = {
+		text: (canvasContext, {text, styles}) =>
+			canvasContext.drawText(text, styles, {stroke: !!styles.strokeWeight}),
 
-	img: (canvasContext, {img, styles}) =>
-		canvasContext.drawImage(img, styles)
-}
-const getTypeof = orig => {
-	if (orig.type == 'text') return drawType.text
-	if (orig.type == 'img') return drawType.img
-	throw new TypeError(`Unknown paint type ${orig.type}`)
-}
-const oldTextTypeToNew = op => ({type: 'text', text: op[0], styles: op[2], use: op[1]})
-// for each img
-argv._.filter(v => !(checkSrcType('css')(v) || checkSrcType('requirable')(v))).forEach(image => {
-	const src = path.normalize('./' + image)
-	const ext = path.extname(src)
-	const base = path.basename(src, ext)
-	// each text profile
-	for(let profile in config) {
-		let canvas = new Canvas(src)
-		const task = config[profile]
-		// each operation
-		task
-		.map(op => Array.isArray(op) ? oldTextTypeToNew(op) : op)
-		.map(op => {
-			op.styles = merge(op.styles, Styles('.' + op.use))
-			return op
-		})
-		.map(op => [getTypeof(op), op])
-		.forEach(([f, op]) => f(canvas, op))
-
-		const outputPattern = profile.replace(/\$/g, base)
-		const base_out = path.basename(outputPattern)
-		const outputDir = path.dirname(outputPattern)
-		const final = path.join(outputDir, base_out + ext)
-		if (!fs.existsSync(outputDir)){
-			console.log('Creating "' + outputDir + '" ...')
-			fs.mkdirSync(outputDir)
-		}
-		console.log('Output: ' + final)
-		canvas.toFile(final)
+		img: (canvasContext, {img, styles}) =>
+			canvasContext.drawImage(img, styles)
 	}
-})
+	const getTypeof = orig => {
+		if (orig.type == 'text') return drawType.text
+		if (orig.type == 'img') return drawType.img
+		throw new TypeError(`Unknown paint type ${orig.type}`)
+	}
+	const oldTextTypeToNew = op => ({type: 'text', text: op[0], styles: op[2], use: op[1]})
+	// for each img
+	argv._.filter(v => !(checkSrcType('css')(v) || checkSrcType('requirable')(v))).forEach(image => {
+		const src = path.normalize('./' + image)
+		const ext = path.extname(src)
+		const base = path.basename(src, ext)
+		// each text profile
+		for(let profile in config) {
+			let canvas = new Canvas(src)
+			const task = config[profile]
+			// each operation
+			task
+			.map(op => Array.isArray(op) ? oldTextTypeToNew(op) : op)
+			.map(op => {
+				op.styles = merge(op.styles, Styles('.' + op.use))
+				return op
+			})
+			.map(op => [getTypeof(op), op])
+			.forEach(([f, op]) => f(canvas, op))
+
+			const outputPattern = profile.replace(/\$/g, base)
+			const base_out = path.basename(outputPattern)
+			const outputDir = path.dirname(outputPattern)
+			const final = path.join(outputDir, base_out + ext)
+			if (!fs.existsSync(outputDir)){
+				console.log('Creating "' + outputDir + '" ...')
+				fs.mkdirSync(outputDir)
+			}
+			console.log('Output: ' + final)
+			canvas.toFile(final)
+		}
+	})
+}
